747f1faddf1d5fe1e7e580e451fba5e01f02c922
Kazuto Murase <kmurase@pfrobotics.jp>
2023-11-19 15:39:41 +0900

    add pages games_on_block

--- pages/games_on_block.js
+++ pages/games_on_block.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import GamesOnBlock from '../components/games_on_block';
 import HokeiManResult from '../components/hokei_man_result';
+import { useRouter } from 'next/router';
 
 const Home = () => {
+    const router = useRouter();
+    const { block_number } = router.query;
+    if (block_number === undefined) {
+        return (<></>);
+    }
     return (
             <>
-            <GamesOnBlock block_number='a' />
+            <GamesOnBlock block_number={block_number} />
             <br />
             <hr />
             <HokeiManResult updateInterval={3000} />
