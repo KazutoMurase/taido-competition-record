dffba8e95fda676cb13ff57496521aacc145713a
Kazuto Murase <kattyokaccho3616@gmail.com>
2023-12-07 02:04:04 +0900

    update ui in test

--- pages/test/check_players_on_block.js
+++ pages/test/check_players_on_block.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import CheckPlayers from '../../components/check_players_on_block';
 import CheckDantai from '../../components/check_dantai_on_block';
+import GetResult from '../../components/get_result';
 import { useRouter } from 'next/router';
+import { GetEventName } from '../../lib/get_event_name';
 
 const Home = () => {
     const router = useRouter();
@@ -10,9 +12,11 @@ const Home = () => {
         return (<></>);
     }
     if (dantai === '0') {
+        const event_name = GetEventName(event_id);
         return (
                 <>
                 <CheckPlayers block_number={block_number} schedule_id={schedule_id} event_id={event_id} update_interval={6000} is_test={true} />
+                <GetResult updateInterval={6000} event_name={event_name} block_number={block_number} />
                 </>
         );
     } else {
