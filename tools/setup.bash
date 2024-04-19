f19af20f8a80a2f316c67af80362c4cfd2829796
Kazuto Murase <kattyokaccho3616@gmail.com>
2024-04-20 01:03:50 +0900

    add setup bash (#7)

---
+++ tools/setup.bash
@@ -0,0 +1,8 @@
+#!/bin/bash
+systemctl start redis-server.service
+
+/etc/init.d/postgresql start
+sudo -u test_user createdb taido_record
+cd /ws/data/2023 && sudo -u test_user psql -d taido_record < generate_tables.sql
+
+cd /ws && npm run dev
