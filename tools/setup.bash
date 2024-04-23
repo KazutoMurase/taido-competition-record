78f17ace0110a288bd6465121c42811960294eec
Kenta Saito <37744457+knt3110@users.noreply.github.com>
2024-04-23 09:45:55 +0900

    fix setup script (#8)
    Co-authored-by: Kazuto Murase <kattyokaccho3616@gmail.com>

--- tools/setup.bash
+++ tools/setup.bash
@@ -5,4 +5,4 @@ systemctl start redis-server.service
 sudo -u test_user createdb taido_record
 cd /ws/data/2023 && sudo -u test_user psql -d taido_record < generate_tables.sql
 
-cd /ws && npm run dev
+cd /ws && npm install && npm run dev
