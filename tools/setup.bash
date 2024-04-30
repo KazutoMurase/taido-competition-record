365cc269f56f1f211d9e5cfd5c391534d67e66ab
Kazuto Murase <kattyokaccho3616@gmail.com>
2024-05-01 00:02:10 +0900

    check database existence

--- tools/setup.bash
+++ tools/setup.bash
@@ -2,7 +2,10 @@
 systemctl start redis-server.service
 
 /etc/init.d/postgresql start
-sudo -u test_user createdb taido_record
-cd /ws/data/2023 && sudo -u test_user psql -d taido_record < generate_tables.sql
-
+if sudo -u test_user psql -lqt | cut -d \| -f 1 | grep -qw taido_record; then
+    echo "database already exists."
+else
+    sudo -u test_user createdb taido_record
+    cd /ws/data/2023 && sudo -u test_user psql -d taido_record < generate_tables.sql
+fi
 cd /ws && npm run dev
