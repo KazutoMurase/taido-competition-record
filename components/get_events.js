80cf75e58f240c39df85d6f0d6ec20f75a3fd645
knt3110 <xpenkenta194@gmail.com>
2024-05-21 02:57:04 +0900

    add get_events api and component

---
+++ components/get_events.js
@@ -0,0 +1,51 @@
+import { useEffect, useState } from "react";
+import { useRouter } from "next/router";
+import Grid from "@mui/material/Grid";
+import Button from "@mui/material/Button";
+import { GetEventName } from "../lib/get_event_name";
+
+function GetEvents() {
+  const router = useRouter();
+  const ToResult = (event_name) => {
+    router.push("/results/" + event_name);
+  };
+
+  const [data, setData] = useState([]);
+
+  const fetchData = async () => {
+    const response = await fetch("/api/get_events");
+    const result = await response.json();
+    setData(result);
+  };
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const events = data.map((item) => {
+    const event_name = GetEventName(item["id"]);
+    // TODO: event_type テーブルに existence を追加して、existence が 1 のもののみ表示する
+    if (event_name === "dantai") {
+      return <></>;
+    }
+    return (
+      <Grid
+        container
+        justifyContent="center"
+        alignItems="center"
+        style={{ height: "60px" }}
+      >
+        <Button
+          variant="contained"
+          type="submit"
+          onClick={(e) => ToResult("zissen_man")}
+        >
+          {item["name"].replace("'", "").replace("'", "")}
+        </Button>
+      </Grid>
+    );
+  });
+
+  return events;
+}
+
+export default GetEvents;
