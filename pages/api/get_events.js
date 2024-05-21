4222307a9d913938464c284a572e286bbd3cae7c
knt3110 <xpenkenta194@gmail.com>
2024-05-21 22:45:16 +0900

    fix for format

--- pages/api/get_events.js
+++ pages/api/get_events.js
@@ -1,17 +1,17 @@
-import GetClient from "../../lib/db_client"
+import GetClient from "../../lib/db_client";
 
 const GetEvents = async (req, res) => {
   try {
     const client = await GetClient();
     const event_name = req.query.event_name;
     const query = "SELECT id, name, existence FROM event_type";
-    const result = await client.query(query)
+    const result = await client.query(query);
     const sorted_data = result.rows.sort((a, b) => a.id - b.id);
     res.json(sorted_data);
   } catch (error) {
     console.log(error);
-    res.status(500).json({ error: "Error feching data" })
+    res.status(500).json({ error: "Error feching data" });
   }
-}
+};
 
 export default GetEvents;
