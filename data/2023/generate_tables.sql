fd5a7ac8e6f32444c25a8e3672a444def0cfb384
knt3110 <xpenkenta194@gmail.com>
2024-05-23 01:24:03 +0900

    add event_type existence column to 2023 data

--- data/2023/generate_tables.sql
+++ data/2023/generate_tables.sql
@@ -6,7 +6,8 @@ primary key(id));
 create table event_type
 (id integer not null,
  name text not null,
-primary key(id));
+ existence integer not null,
+ primary key(id));
 
 create table court_type
 (id integer not null,
