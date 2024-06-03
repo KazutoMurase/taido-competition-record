8db2818be0f69718affb0d1a24b2f032f0d760ac
Kazuto Murase <kattyokaccho3616@gmail.com>
2024-06-03 09:56:21 +0900

    try to use env variables from cloud run configuration

--- tools/setup.bash
+++ tools/setup.bash
@@ -3,4 +3,15 @@ competition_name=$1
 
 systemctl start redis-server.service
 
+if [ -z "${PRODUCTION}" ]; then
+    /etc/init.d/postgresql start
+    if sudo -u postgres psql -lqt | cut -d \| -f 1 | grep -qw taido_record; then
+        echo "database already exists."
+    else
+        sudo -u postgres createdb taido_record
+        cd /ws/data/$competition_name && sudo -u postgres psql -d taido_record < generate_tables.sql
+        cd /ws/data/test && sudo -u postgres psql -d taido_record < generate_tables.sql
+    fi
+fi
+
 cd /ws && npm install && npm run dev
