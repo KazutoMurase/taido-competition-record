8338470671419b98241b04e6b466afbfa0a9b475
Shintaro Komatsu <167593243+shintarokkkk@users.noreply.github.com>
2024-06-10 06:36:04 +0900

    add dantai_zissen in test database (#84)

--- data/test/generate_tables.sql
+++ data/test/generate_tables.sql
@@ -1,5 +1,141 @@
+create table test_groups
+(id integer not null,
+name text not null,
+primary key(id));
+
+create table test_players
+(id integer not null,
+group_id integer not null,
+name text not null,
+name_kana text not null,
+test_zissen_man_player_id integer unique,
+test_hokei_man_player_id integer unique,
+test_zissen_woman_player_id integer unique,
+test_hokei_woman_player_id integer unique,
+test_hokei_sonen_player_id integer unique,
+primary key(id),
+foreign key (group_id) references test_groups(id));
+
+create table test_notification_request
+(id serial not null,
+ event_id integer not null,
+ player_id integer unique,
+ group_id integer,
+ group_name text,
+ court_id integer not null,
+ primary key(id),
+ foreign key (event_id) references event_type(id),
+ foreign key (player_id) references test_players(id),
+ foreign key (court_id) references court_type(id));
+
+\copy test_groups from 'test_groups.csv' csv header;
+\copy test_players from 'test_players.csv' csv header;
+
 insert into court_type(id,name) values (100, 'Xコート');
 insert into court_type(id,name) values (101, 'Yコート');
+
+create table test_hokei_man
+(id integer not null,
+left_player_id integer,
+right_player_id integer,
+next_left_id integer,
+next_right_id integer,
+left_player_flag integer,
+left_retire integer,
+right_retire integer,
+foreign key (left_player_id) references test_players(test_hokei_man_player_id),
+foreign key (right_player_id) references test_players(test_hokei_man_player_id),
+primary key(id));
+
+create table test_zissen_man
+(id integer not null,
+left_player_id integer,
+right_player_id integer,
+next_left_id integer,
+next_right_id integer,
+left_player_flag integer,
+left_retire integer,
+right_retire integer,
+foreign key (left_player_id) references test_players(test_zissen_man_player_id),
+foreign key (right_player_id) references test_players(test_zissen_man_player_id),
+primary key(id));
+
+create table test_hokei_woman
+(id integer not null,
+left_player_id integer,
+right_player_id integer,
+next_left_id integer,
+next_right_id integer,
+left_player_flag integer,
+left_retire integer,
+right_retire integer,
+foreign key (left_player_id) references test_players(test_hokei_woman_player_id),
+foreign key (right_player_id) references test_players(test_hokei_woman_player_id),
+primary key(id));
+
+create table test_zissen_woman
+(id integer not null,
+left_player_id integer,
+right_player_id integer,
+next_left_id integer,
+next_right_id integer,
+left_player_flag integer,
+left_retire integer,
+right_retire integer,
+foreign key (left_player_id) references test_players(test_zissen_woman_player_id),
+foreign key (right_player_id) references test_players(test_zissen_woman_player_id),
+primary key(id));
+
+\COPY test_hokei_man from 'test_hokei_man.csv' csv header;
+\COPY test_zissen_man from 'test_zissen_man.csv' csv header;
+\COPY test_hokei_woman from 'test_hokei_woman.csv' csv header;
+\COPY test_zissen_woman from 'test_zissen_woman.csv' csv header;
+
+create table test_dantai_zissen_man_groups
+(id integer not null,
+group_id integer not null,
+name text not null,
+foreign key (group_id) references test_groups(id),
+primary key(id));
+
+create table test_dantai_zissen_woman_groups
+(id integer not null,
+group_id integer not null,
+name text not null,
+foreign key (group_id) references test_groups(id),
+primary key(id));
+
+create table test_dantai_zissen_man
+(id integer not null,
+left_group_id integer,
+right_group_id integer,
+next_left_id integer,
+next_right_id integer,
+left_group_flag integer,
+left_retire integer,
+right_retire integer,
+foreign key (left_group_id) references dantai_zissen_man_groups(id),
+foreign key (right_group_id) references dantai_zissen_man_groups(id),
+primary key(id));
+
+create table test_dantai_zissen_woman
+(id integer not null,
+left_group_id integer,
+right_group_id integer,
+next_left_id integer,
+next_right_id integer,
+left_group_flag integer,
+left_retire integer,
+right_retire integer,
+foreign key (left_group_id) references test_dantai_zissen_woman_groups(id),
+foreign key (right_group_id) references test_dantai_zissen_woman_groups(id),
+primary key(id));
+
+\copy test_dantai_zissen_man_groups from 'test_dantai_zissen_man_groups.csv' csv header;
+\copy test_dantai_zissen_woman_groups from 'test_dantai_zissen_woman_groups.csv' csv header;
+
+\copy test_dantai_zissen_man from 'test_dantai_zissen_man.csv' csv header;
+\copy test_dantai_zissen_woman from 'test_dantai_zissen_woman.csv' csv header;
 
 create table block_x
 (id integer not null,
@@ -57,96 +193,13 @@ insert into current_block_x(id, game_id) values (1, 1);
 \COPY block_y_games from 'block_y_games.csv' csv header;
 insert into current_block_y(id, game_id) values (1, 1);
 
-create table test_players
-(id integer not null,
-group_id integer not null,
-name text not null,
-name_kana text not null,
-test_zissen_man_player_id integer unique,
-test_hokei_man_player_id integer unique,
-test_zissen_woman_player_id integer unique,
-test_hokei_woman_player_id integer unique,
-test_hokei_sonen_player_id integer unique,
-primary key(id),
-foreign key (group_id) references groups(id));
-
-create table test_hokei_man
-(id integer not null,
-left_player_id integer,
-right_player_id integer,
-next_left_id integer,
-next_right_id integer,
-left_player_flag integer,
-left_retire integer,
-right_retire integer,
-foreign key (left_player_id) references test_players(test_hokei_man_player_id),
-foreign key (right_player_id) references test_players(test_hokei_man_player_id),
-primary key(id));
-
-create table test_zissen_man
-(id integer not null,
-left_player_id integer,
-right_player_id integer,
-next_left_id integer,
-next_right_id integer,
-left_player_flag integer,
-left_retire integer,
-right_retire integer,
-foreign key (left_player_id) references test_players(test_zissen_man_player_id),
-foreign key (right_player_id) references test_players(test_zissen_man_player_id),
-primary key(id));
-
-create table test_hokei_woman
-(id integer not null,
-left_player_id integer,
-right_player_id integer,
-next_left_id integer,
-next_right_id integer,
-left_player_flag integer,
-left_retire integer,
-right_retire integer,
-foreign key (left_player_id) references test_players(test_hokei_woman_player_id),
-foreign key (right_player_id) references test_players(test_hokei_woman_player_id),
-primary key(id));
-
-create table test_zissen_woman
-(id integer not null,
-left_player_id integer,
-right_player_id integer,
-next_left_id integer,
-next_right_id integer,
-left_player_flag integer,
-left_retire integer,
-right_retire integer,
-foreign key (left_player_id) references test_players(test_zissen_woman_player_id),
-foreign key (right_player_id) references test_players(test_zissen_woman_player_id),
-primary key(id));
-
-\COPY test_players from 'test_players.csv' csv header;
-\COPY test_hokei_man from 'test_hokei_man.csv' csv header;
-\COPY test_zissen_man from 'test_zissen_man.csv' csv header;
-\COPY test_hokei_woman from 'test_hokei_woman.csv' csv header;
-\COPY test_zissen_woman from 'test_zissen_woman.csv' csv header;
-
-create table test_notification_request
-(id serial not null,
- event_id integer not null,
- player_id integer unique,
- group_id integer,
- group_name text,
- court_id integer not null,
- primary key(id),
- foreign key (event_id) references event_type(id),
- foreign key (player_id) references test_players(id),
- foreign key (court_id) references court_type(id));
-
 create table test_dantai
 (id integer not null,
  event_id integer not null,
  game_id integer not null,
  group_id integer not null,
  foreign key (event_id) references event_type(id),
- foreign key (group_id) references groups(id),
+ foreign key (group_id) references test_groups(id),
  primary key(id));
 
 \COPY test_dantai from 'test_dantai.csv' csv header;
