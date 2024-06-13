6761db4a79d521141719a7fb44c644c45c3f6601
Kazuto Murase <kattyokaccho3616@gmail.com>
2024-06-13 21:36:01 +0900

    add button to notification request page

--- pages/summary.tsx
+++ pages/summary.tsx
@@ -1,7 +1,14 @@
 import React from "react";
+import Button from "@mui/material/Button";
+import Grid from "@mui/material/Grid";
+import { useRouter } from "next/router";
 import GetSummary from "../components/get_summary";
 
 const Summary: React.FC = ({}) => {
+  const router = useRouter();
+  const ToBack = () => {
+    router.back();
+  };
   // TODO: use api/get_events
   const event_ids = [1, 2, 3, 4, 12, 13, 14, 15, 16, 17];
   return (
@@ -9,6 +16,16 @@ const Summary: React.FC = ({}) => {
       {event_ids.map((id) => (
         <GetSummary key={id} event_id={id} />
       ))}
+      <Grid
+        container
+        justifyContent="center"
+        alignItems="center"
+        style={{ height: "80px" }}
+      >
+        <Button variant="contained" type="submit" onClick={(e) => ToBack()}>
+          戻る
+        </Button>
+      </Grid>
     </>
   );
 };
