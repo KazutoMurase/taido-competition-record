d18c0091a0f4298c2d18c988dcafcb004b038c40
Kazuto Murase <kattyokaccho3616@gmail.com>
2024-07-13 00:58:27 +0900

    add summary page to top (#132)

--- tools/setup.bash
+++ tools/setup.bash
@@ -1,5 +1,4 @@
 #!/bin/bash
-competition_name=$1
 
 if [ "${USE_LOCAL_REDIS}" == "1" ]; then
     systemctl start redis-server.service
@@ -11,7 +10,7 @@ if [ "${USE_LOCAL_DB}" == "1" ]; then
         echo "database already exists."
     else
         sudo -u postgres createdb taido_record
-        cd /ws/data/$competition_name && sudo -u postgres psql -d taido_record < generate_tables.sql
+        cd /ws/data/$COMPETITION_NAME && sudo -u postgres psql -d taido_record < generate_tables.sql
         cd /ws/data/test && sudo -u postgres psql -d taido_record < generate_tables.sql
     fi
 fi
