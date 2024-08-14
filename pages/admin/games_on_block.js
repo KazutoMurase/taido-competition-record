0e5b481e117211f30ffc4fa8ca40b3b93c5f9425
Kazuto Murase <kattyokaccho3616@gmail.com>
2024-08-14 20:40:49 +0900

    get table progress on block

--- pages/admin/games_on_block.js
+++ pages/admin/games_on_block.js
@@ -1,6 +1,8 @@
 import React from "react";
 import GamesOnBlock from "../../components/games_on_block";
+import TableProgressOnBlock from "../../components/table_progress_on_block";
 import GetResult from "../../components/get_result";
+import GetTableResult from "../../components/get_table_result";
 import { useRouter } from "next/router";
 import { GetEventName } from "../../lib/get_event_name";
 
@@ -11,22 +13,41 @@ const Home = () => {
     return <></>;
   }
   const event_name = GetEventName(event_id);
-  return (
-    <>
-      <GamesOnBlock
-        block_number={block_number}
-        event_name={event_name}
-        schedule_id={schedule_id}
-        update_interval={3000}
-      />
-      <br />
-      <GetResult
-        updateInterval={3000}
-        event_name={event_name}
-        block_number={block_number}
-      />
-    </>
-  );
+  if (event_name.includes("dantai_hokei")) {
+    return (
+      <>
+        <TableProgressOnBlock
+          block_number={block_number}
+          event_name={event_name}
+          schedule_id={schedule_id}
+          update_interval={3000}
+        />
+        <br />
+        <GetTableResult
+          updateInterval={3000}
+          event_name={event_name}
+          block_number={block_number}
+        />
+      </>
+    );
+  } else {
+    return (
+      <>
+        <GamesOnBlock
+          block_number={block_number}
+          event_name={event_name}
+          schedule_id={schedule_id}
+          update_interval={3000}
+        />
+        <br />
+        <GetResult
+          updateInterval={3000}
+          event_name={event_name}
+          block_number={block_number}
+        />
+      </>
+    );
+  }
 };
 
 export default Home;
