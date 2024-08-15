af94628f4c8f662405d1918926b1d9e9828ac1bf
Kazuto Murase <kattyokaccho3616@gmail.com>
2024-08-16 01:57:35 +0900

    Add 2024 adult data (#140)

---
+++ pages/results/zissen_sonen_man.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { InferGetServerSidePropsType, GetServerSideProps } from "next";
+import GetResult from "../../components/get_result";
+
+export const getServerSideProps = async (context) => {
+  const params = { production_test: process.env.PRODUCTION_TEST };
+  return {
+    props: { params },
+  };
+};
+
+const Home = ({ params }) => {
+  const hide = params.production_test === "1";
+  return (
+    <>
+      <GetResult event_name="zissen_sonen_man" hide={hide} />
+    </>
+  );
+};
+
+export default Home;
