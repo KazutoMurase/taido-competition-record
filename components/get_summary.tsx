bb785540edf90e3cf31758e21d28a3f9bb92f8d5
Kazuto Murase <kattyokaccho3616@gmail.com>
2024-09-08 01:20:01 +0900

    use get_events in summary

--- components/get_summary.tsx
+++ components/get_summary.tsx
@@ -4,42 +4,12 @@ import Grid from "@mui/material/Grid";
 import Summary from "./show_summary";
 import { GetEventName } from "../lib/get_event_name";
 
-// TODO: 実大会競技名が固まったら別ファイルにMap作成
-const test_event_id_vs_event_name: Map<number, string> = new Map([
-  [0, "全日程終了"],
-  [1, "男子個人実戦"],
-  [2, "男子個人法形"],
-  [3, "女子個人実戦"],
-  [4, "女子個人法形"],
-  [5, "壮年法形"],
-  [6, "女子団体実戦"],
-  [7, "男子団体実戦"],
-  [8, "男子団体法形"],
-  [9, "女子団体法形"],
-  [10, "男子団体展開"],
-  [11, "女子団体展開"],
-  [12, "個人法形　新人"],
-  [13, "男子級位個人実戦"],
-  [14, "男子級位個人法形"],
-  [15, "女子級位個人実戦"],
-  [16, "女子級位個人法形"],
-  [17, "団体実戦"],
-  [18, "団体法形"],
-  [19, "団体展開"],
-  [20, "個人法形　級位"],
-  [21, "壮年実戦　男子"],
-  [22, "壮年実戦　女子"],
-  [23, "個人法形　制の法形"],
-  [24, "命法形（有段者）"],
-  [25, "命法形（新人級位）"],
-]);
-
-const GetSummary: React.FC<{ event_id: number; hide: boolean }> = ({
-  event_id,
-  hide,
-}) => {
+const GetSummary: React.FC<{
+  event_id: number;
+  event_name: string;
+  hide: boolean;
+}> = ({ event_id, event_name, hide }) => {
   const [data, setData] = useState({});
-  const event_name = GetEventName(event_id);
   useEffect(() => {
     async function fetchData() {
       if (
@@ -83,7 +53,7 @@ const GetSummary: React.FC<{ event_id: number; hide: boolean }> = ({
         alignItems="center"
         style={{ height: "30px" }}
       >
-        {test_event_id_vs_event_name.get(event_id)}
+        {event_name}
       </Grid>
       <Summary winners={data} />
     </>
