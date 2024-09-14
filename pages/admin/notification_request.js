4e37a19520c5d4e9790bbe3277e30dd43a855298
Kazuto Murase <kattyokaccho3616@gmail.com>
2024-09-15 02:15:19 +0900

    mobile page for shikai

--- pages/admin/notification_request.js
+++ pages/admin/notification_request.js
@@ -1,8 +1,18 @@
 import React from "react";
 import NotificationRequest from "../../components/notification_request";
+import { useMediaQuery } from "@mui/material";
+import { useTheme } from "@mui/material/styles";
 
 const Home = () => {
-  return <NotificationRequest update_interval={3000} return_url="/admin" />;
+  const theme = useTheme();
+  const isMobile = useMediaQuery(theme.breakpoints.down("sm"));
+  return (
+    <NotificationRequest
+      update_interval={3000}
+      is_mobile={isMobile}
+      return_url="/admin"
+    />
+  );
 };
 
 export default Home;
