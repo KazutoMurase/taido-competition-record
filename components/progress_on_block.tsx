a85f5e654d66303cbbad6e68894d673014583315
Kazuto Murase <kattyokaccho3616@gmail.com>
2024-11-16 11:02:52 +0900

    remove test_event_id_vs_event_name in progress on block (#246)

--- components/progress_on_block.tsx
+++ components/progress_on_block.tsx
@@ -25,37 +25,6 @@ interface TimeScheduleData {
   players_checked: number;
 }
 
-// TODO: 実大会競技名が固まったら別ファイルにMap作成
-const test_event_id_vs_event_name: Map<number, string> = new Map([
-  [0, "全日程終了"],
-  [1, "段位男子実戦"],
-  [2, "段位男子法形"],
-  [3, "段位女子実戦"],
-  [4, "段位女子法形"],
-  [5, "壮年法形"],
-  [6, "女子団体実戦"],
-  [7, "男子団体実戦"],
-  [8, "男子団体法形"],
-  [9, "女子団体法形"],
-  [10, "男子団体展開"],
-  [11, "女子団体展開"],
-  [12, "新人法形"],
-  [13, "男子級位個人実戦"],
-  [14, "男子級位個人法形"],
-  [15, "女子級位個人実戦"],
-  [16, "女子級位個人法形"],
-  [17, "団体実戦"],
-  [18, "団体法形"],
-  [19, "団体展開"],
-  [20, "級位法形"],
-  [21, "壮年男子実戦"],
-  [22, "壮年女子実戦"],
-  [23, "制の法形"],
-  [24, "命 段位法形"],
-  [25, "命 新人級位"],
-  [26, "新人団体法形"],
-]);
-
 function GetGamesText(schedule) {
   if (!schedule.games_text) {
     return "";
@@ -139,16 +108,14 @@ const ProgressOnBlock: React.FC<{
         >
           <td>{schedule.time_schedule?.replace(/['"]+/g, "")}</td>
           <td>
-            {GetEventName(schedule.event_id) === "dantai" ? (
-              <>{test_event_id_vs_event_name.get(schedule.event_id)}</>
-            ) : (
+            {
               <a
                 className="color-disabled"
                 href={"results/" + GetEventName(schedule.event_id)}
               >
-                {test_event_id_vs_event_name.get(schedule.event_id)}
+                {schedule.name?.replace(/['"]+/g, "")}
               </a>
-            )}
+            }
           </td>
           <td>{GetGamesText(schedule)}</td>
           <td>
