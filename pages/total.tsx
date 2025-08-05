50d964a5d03902f67e3d0597c25f84f47423bdc8
Kazuto Murase <kattyokaccho3616@gmail.com>
2025-08-05 10:55:14 +0900

    Update total page for kids (#297)

--- pages/total.tsx
+++ pages/total.tsx
@@ -17,29 +17,101 @@ export const getServerSideProps = async (context) => {
   };
 };
 
-const Total: React.FC<{ params }> = ({ params }) => {
-  const router = useRouter();
-  const ToBack = () => {
-    router.back();
-  };
-  const [events, setEvents] = useState([]);
-
-  const fetchEvents = async () => {
-    const response = await fetch("/api/get_events");
-    const result = await response.json();
-    setEvents(result);
-  };
-  useEffect(() => {
-    fetchEvents();
-  }, []);
-  // Perhaps score definition should be statics or database in future
-  const use_different_personal_scores = params.title.includes("全国学生");
-  const event_ids = [1, 2, 3, 4, 5, 7, 8, 10, 6, 9, 11, 26];
-  let personal_span = 0;
-  let dantai_span = 0;
+function GenerateItemsForKids(events, event_ids, header1, header2, spans) {
   let row_span = 1;
-  let header1 = [];
-  let header2 = [];
+  let human_type = "";
+  let competition_type = "";
+  let same_human_type_length = 0;
+  for (const id of event_ids) {
+    let event_info = events.find((item) => item.id === id);
+    if (!event_info || !event_info.existence) {
+      continue;
+    }
+    const event_name = GetEventName(id);
+    if (event_name.includes("dantai") || event_name.includes("tenkai")) {
+      spans[1] += 1;
+    } else {
+      spans[0] += 1;
+    }
+    if (event_name.includes("woman")) {
+      if (human_type === "女子") {
+        same_human_type_length += 1;
+      } else if (same_human_type_length >= 1) {
+        header1.push(
+          <td
+            style={{ width: "50px", padding: 2 }}
+            colSpan={same_human_type_length}
+            rowSpan={row_span}
+          >
+            {human_type + competition_type}
+          </td>,
+        );
+        same_human_type_length = 1;
+      } else {
+        same_human_type_length = 1;
+      }
+      human_type = "女子";
+      competition_type = event_name.includes("zissen") ? "実戦" : "法形";
+      row_span = 1;
+    } else if (event_name.includes("man")) {
+      if (human_type === "男子") {
+        same_human_type_length += 1;
+      } else if (same_human_type_length >= 1) {
+        header1.push(
+          <td
+            style={{ width: "50px", padding: 2 }}
+            colSpan={same_human_type_length}
+            rowSpan={row_span}
+          >
+            {human_type + competition_type}
+          </td>,
+        );
+        same_human_type_length = 1;
+      } else {
+        same_human_type_length = 1;
+      }
+      human_type = "男子";
+      competition_type = event_name.includes("zissen") ? "実戦" : "法形";
+      row_span = 1;
+    }
+    if (row_span === 2) {
+      // do nothing
+    } else if (event_name.includes("junior_high")) {
+      header2.push(<td style={{ width: "50px", padding: 2 }}>中学</td>);
+    } else if (event_name.includes("higher_grades")) {
+      header2.push(<td style={{ width: "50px", padding: 2 }}>小高</td>);
+    } else if (event_name.includes("lower_grades")) {
+      header2.push(<td style={{ width: "50px", padding: 2 }}>小低</td>);
+    } else {
+      header1.push(
+        <td
+          style={{ width: "50px", padding: 2 }}
+          colSpan={same_human_type_length}
+          rowSpan={2}
+        >
+          {event_name.includes("dantai_zissen") ? (
+            <>
+              団体
+              <br />
+              実戦
+            </>
+          ) : event_name.includes("tenkai") ? (
+            "展開"
+          ) : (
+            <>
+              団体
+              <br />
+              法形
+            </>
+          )}
+        </td>,
+      );
+    }
+  }
+}
+
+function GenerateItems(events, event_ids, header1, header2, spans) {
+  let row_span = 1;
   let human_type = "";
   let same_human_type_length = 0;
   for (const id of event_ids) {
@@ -49,9 +121,9 @@ const Total: React.FC<{ params }> = ({ params }) => {
     }
     const event_name = GetEventName(id);
     if (event_name.includes("dantai") || event_name.includes("tenkai")) {
-      dantai_span += 1;
+      spans[1] += 1;
     } else {
-      personal_span += 1;
+      spans[0] += 1;
     }
     if (event_name.includes("sonen")) {
       if (human_type === "壮年") {
@@ -159,6 +231,38 @@ const Total: React.FC<{ params }> = ({ params }) => {
       </td>,
     );
   }
+}
+
+const Total: React.FC<{ params }> = ({ params }) => {
+  const router = useRouter();
+  const ToBack = () => {
+    router.back();
+  };
+  const [events, setEvents] = useState([]);
+
+  const fetchEvents = async () => {
+    const response = await fetch("/api/get_events");
+    const result = await response.json();
+    setEvents(result);
+  };
+  useEffect(() => {
+    fetchEvents();
+  }, []);
+  // Perhaps score definition should be statics or database in future
+  const use_different_personal_scores = params.title.includes("全国学生");
+  const is_kids_competition = params.title.includes("少年少女");
+  const event_ids = is_kids_competition
+    ? [33, 29, 35, 31, 34, 30, 27, 36, 32, 28, 7, 18, 19]
+    : [1, 2, 3, 4, 5, 7, 8, 10, 6, 9, 11, 26];
+  // personal, dantai
+  let spans = [0, 0];
+  let header1 = [];
+  let header2 = [];
+  if (is_kids_competition) {
+    GenerateItemsForKids(events, event_ids, header1, header2, spans);
+  } else {
+    GenerateItems(events, event_ids, header1, header2, spans);
+  }
   const [data, setData] = useState([]);
   useEffect(() => {
     async function fetchData() {
@@ -183,10 +287,10 @@ const Total: React.FC<{ params }> = ({ params }) => {
                   <td style={{ width: "150px", padding: 2 }} rowSpan={3}>
                     地区名
                   </td>
-                  <td colSpan={personal_span} style={{ padding: 2 }}>
+                  <td colSpan={spans[0]} style={{ padding: 2 }}>
                     個人種目競技
                   </td>
-                  <td colSpan={dantai_span} style={{ padding: 2 }}>
+                  <td colSpan={spans[1]} style={{ padding: 2 }}>
                     団体種目競技
                   </td>
                   <td rowSpan={3} style={{ width: "70px", padding: 2 }}>
