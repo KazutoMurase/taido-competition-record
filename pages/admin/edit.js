03cde93c270b8601fa987d9d858a245db066f233
Kazuto Murase <murase@oceanic-constellations.com>
2026-06-24 01:53:53 +0900

    edit tournament page

---
+++ pages/admin/edit.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { useRouter } from "next/router";
+import { Alert, Box } from "@mui/material";
+import TournamentEditor from "../../components/tournament_editor";
+
+export default function EditTournamentPage() {
+  const router = useRouter();
+  const { competition, event_name } = router.query;
+
+  if (!router.isReady) {
+    return null;
+  }
+
+  if (!competition || !event_name) {
+    return (
+      <Box sx={{ p: 2 }}>
+        <Alert severity="info">
+          competition と event_name を指定してください。例:
+          /admin/edit?competition=2025_kid&event_name=hokei_man
+        </Alert>
+      </Box>
+    );
+  }
+
+  return (
+    <TournamentEditor
+      competition={String(competition)}
+      eventName={String(event_name)}
+    />
+  );
+}
